1d8acc24a4c256cd9162d9ce19e8ecd85200fc9b
chinaxunmei <3812680591@qq.com>
2026-08-13 14:24:48 +0800

    fix: 添加 __init__.py 修复 ComfyUI 扩展识别问题

---
+++ __init__.py
@@ -0,0 +1,7 @@
+"""ComfyUI-Erase-Watermarks：面向 ComfyUI 工作流的自定义去水印扩展"""
+
+#from .nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+from .nodes import NODE_CLASS_MAPPINGS
+
+#__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
+__all__ = ["NODE_CLASS_MAPPINGS"]
